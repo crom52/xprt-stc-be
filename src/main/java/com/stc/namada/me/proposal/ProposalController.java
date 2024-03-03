12fbabdd2c8a1f0c74a94d2167085accb7e5cd7d
th.phuc <tranhoanphuc.vn@smilegate.com>
2024-03-03 18:45:40 +0700

    remove login

--- src/main/java/com/stc/namada/me/proposal/ProposalController.java
+++ src/main/java/com/stc/namada/me/proposal/ProposalController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/stc/namada-me")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class NamadaController {
+public class ProposalController {
     ObjectMapper objectMapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
 
     @GetMapping("/proposals")
