ed8466f36d463b17db3e8636cdd2c0a31772cd40
th.phuc <tranhoanphuc.vn@smilegate.com>
2024-04-10 23:15:43 +0700

    cors

--- src/main/java/com/stc/namada/me/config/WebMvcConfig.java
+++ src/main/java/com/stc/namada/me/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("localhost:8282", "http://jecbox.com", "https://jecbox.com", "localhost:5000", "localhost:5000", "https://namada-me-be-0c5a9f264e40.herokuapp.com", "http://namada-me-be-0c5a9f264e40.herokuapp.com")
+                .allowedOrigins("localhost:8282", "http://jecbox.com", "https://jecbox.com", "localhost:5000", "localhost:5000", "https://namada-me-be-0c5a9f264e40.herokuapp.com", "http://namada-me-be-0c5a9f264e40.herokuapp.com", "23.88.126.45:8080", "23.88.126.45")
                 .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
