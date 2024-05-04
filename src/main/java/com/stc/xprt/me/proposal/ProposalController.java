8123c054dbb26e5f648a5489c77e7df2e3f6b786
th.phuc <tranhoanphuc.vn@smilegate.com>
2024-05-04 21:16:39 +0700

    feat:api proposal list

--- src/main/java/com/stc/xprt/me/proposal/ProposalController.java
+++ src/main/java/com/stc/xprt/me/proposal/ProposalController.java
@@ -1,22 +1,46 @@
-//package com.stc.xprt.me.proposal;
-//
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-//import lombok.AccessLevel;
-//import lombok.experimental.FieldDefaults;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/stc/namada-me")
-//@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-//public class ProposalController {
-//    ObjectMapper objectMapper = new ObjectMapper().setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
-//
-//    @GetMapping("/proposals")
-//    public ProposalResponse getProposals() throws JsonProcessingException {
-//        return objectMapper.readValue(ProposalMockData.proposal, ProposalResponse.class);
-//    }
-//}
+package com.stc.xprt.me.proposal;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/stc/xprt")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class ProposalController {
+    @Value("${indexer.rest.url}")
+    String xprtUrl;
+
+    final RestTemplate restTemplate;
+
+
+    @GetMapping("/proposals")
+    public Object getProposals(
+            @RequestParam(value = "proposal_status", defaultValue = "PROPOSAL_STATUS_UNSPECIFIED") String proposalStatus,
+            @RequestParam(required = false, value = "voter") String voter,
+            @RequestParam(required = false, value = "depositor") String depositor,
+            @RequestParam(required = false, value = "key") String key,
+            @RequestParam(required = false, value = "num", defaultValue = "10") Integer num,
+            @RequestParam(required = false, value = "offset", defaultValue = "0") Integer offset) {
+        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromHttpUrl(xprtUrl).path("cosmos/gov/v1/proposals")
+                                                              .queryParam("voter", voter)
+                                                              .queryParam("depositor", depositor).queryParam("key", key)
+                                                              .queryParam("proposal_status", proposalStatus)
+                                                              .queryParam("pagination.offset", offset)
+                                                              .queryParam("pagination.limit", num)
+                                                              .queryParam("pagination.count_total", true)
+                                                              .queryParam("pagination.reverse", true);
+
+        return restTemplate.getForObject(urlBuilder.toUriString(), Map.class);
+    }
+}
