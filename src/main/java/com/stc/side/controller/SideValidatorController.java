1e82cc4b95eb580ef7e05a70f4a84b92f818cf58
th.phuc <tranhoanphuc.vn@smilegate.com>
2024-06-17 23:13:30 +0700

    side- validators

---
+++ src/main/java/com/stc/side/controller/SideValidatorController.java
@@ -0,0 +1,70 @@
+package com.stc.side.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RequestMapping("/stc/side")
+@RestController
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+public class SideValidatorController {
+    final RestTemplate restTemplate;
+
+    static Map<String, Object> validatorCacheMap = new ConcurrentHashMap<>();
+
+    @GetMapping("/validators")
+    public Object getValidators(@RequestParam(defaultValue = "10") String num,
+                                @RequestParam(defaultValue = "0") String offset,
+                                @RequestParam(required = false) String status) {
+
+        String cacheKey = "validators" + num + offset;
+
+        if(validatorCacheMap.containsKey(cacheKey)) {
+            return validatorCacheMap.get(cacheKey);
+        }
+
+        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromHttpUrl("http://49.13.123.160:1317/cosmos/staking/v1beta1/validators")
+                                                              .queryParam("status", status)
+                                                              .queryParam("pagination.count_total", true)
+                                                              .queryParam("pagination.reverse", true)
+                                                              .queryParam("pagination.limit", num)
+                                                              .queryParam("pagination.offset", offset);
+
+
+        var validators = restTemplate.getForObject(urlBuilder.toUriString(), Map.class);
+        validatorCacheMap.put(cacheKey, validators);
+        return validators;
+    }
+
+    @GetMapping("/validator/{id}")
+    public Object getValidatorById(@PathVariable String id) {
+
+        if (validatorCacheMap.containsKey(id)) {
+            return validatorCacheMap.get(id);
+        }
+
+        UriComponentsBuilder urlBuilder = UriComponentsBuilder.fromHttpUrl("http://49.13.123.160:1317/cosmos/staking/v1beta1/validators/").path(id);
+        var validatorDetail = restTemplate.getForObject(urlBuilder.toUriString(), Map.class);
+        validatorCacheMap.put(id, validatorDetail);
+        return validatorDetail;
+    }
+
+    @Scheduled(fixedDelay = 900000)
+    public void clearValidatorCache() {
+        validatorCacheMap.clear();
+        System.out.println("Validator cache cleared.");
+    }
+}
